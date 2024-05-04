6828ffea4c9defa8a9c4dc956fd906df44077afd
nikhil bishnoi <nikhilbishnoibhadu@gmail.com>
2024-05-04 15:50:59 +0530

    first commit

---
+++ assest/js/main.js
@@ -0,0 +1,6 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+  }
+  
